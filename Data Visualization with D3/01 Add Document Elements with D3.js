264f3ea95894d220922674cdd05824af0cd01c02
ryspnc <ryspnc@users.noreply.github.com>
2017-02-09 20:59:40 -0500

    Create 01 Add Document Elements with D3.js

---
+++ Data Visualization with D3/01 Add Document Elements with D3.js
@@ -0,0 +1,12 @@
+/*
+Use the select method to select the body tag in the document. 
+Then append an h1 tag to it, and add the text "Learning D3" into the h1 element.
+*/
+
+<body>
+  <script>
+    d3.select("body")
+      .append("h1")
+      .text("Learning D3")
+  </script>
+</body
